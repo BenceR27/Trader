ca971e7c1928d616759747d5865baa6ef87c88b7
Bencuska <rizb@kkszki.hu>
2025-10-08 08:25:19 +0200

    Database connection created

---
+++ Trader/Connect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trader
+{
+    internal class Connect
+    {
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _password;
+
+    }
+}
